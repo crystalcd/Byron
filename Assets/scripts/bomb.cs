4c82d304c0cf0a02fa14b7418aecfbd3a9029618
crystal <1627566612@qq.com>
2023-08-21 23:46:14 +0800

    111

--- Assets/scripts/bomb.cs
+++ Assets/scripts/bomb.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class bomb : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target != null)
+        {
+            Vector2 directionToTarget = (target.position - transform.position).normalized;
+        }
     }
 }
